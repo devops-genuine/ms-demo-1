b5ee5d7a3cdcfabf999e21c36d1ee30a7c4cdc4b
Jesada Gonkratoke <selfieblue@gmail.com>
2019-10-13 15:30:32 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,6 +1,10 @@
-mkdir src/test/resources
-cp server.truststore.jks src/test/resources/
+keytool -import -alias teiid -file ~/vault-ca.crt -storetype JKS -keystore server.truststore.jks
+mkdir -p src/main/resources
+rm -rf src/main/resources/server.truststore.jks
+cp server.truststore.jks src/main/resources/
 
-java -jar -Dspring.profiles.active=develop target/ms-demo-0.0.1-SNAPSHOT.jar \
-  -Djavax.net.ssl.trustStore=server.truststore.jks \
-  -Djavax.net.ssl.trustStorePassword=changeit
+java -jar -Dspring.profiles.active=develop target/ms-demo-0.0.1-SNAPSHOT.jar
+
+# java -jar -Dspring.profiles.active=develop target/ms-demo-0.0.1-SNAPSHOT.jar \
+#   -Djavax.net.ssl.trustStore=server.truststore.jks \
+#   -Djavax.net.ssl.trustStorePassword=changeit
